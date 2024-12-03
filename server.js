e27a58fd849db57a455f719d935567f6df5fe8f3
YagTur123 <yagiz.efe.turan111@gmail.com>
2024-12-03 23:53:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Perfume = require('./models/perfume'); // Modeli içe aktar
+const Perfume = require('./models/perfume');
 
 const app = express();
 
 // MongoDB URI
-const uri = "mongodb+srv://yagiz:<db_yagiz123>@cluster0.vrkqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = 'mongodb+srv://yagiz:<db_yagiz123>@cluster0.vrkqh.mongodb.net/?retryWrites=true&w=majority';
 
-// Veritabanına bağlan
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB bağlantısı başarılı!'))
-.catch(err => console.error('MongoDB bağlantı hatası:', err));
+// MongoDB'ye bağlan
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB bağlantısı başarılı!'))
+  .catch(err => console.error('MongoDB bağlantı hatası:', err));
 
-// JSON verileri parse etme
 app.use(express.json());
 
 // Form verilerini kaydetme endpoint'i
@@ -28,9 +24,6 @@ app.post('/api/add-perfume', async (req, res) => {
     res.status(500).send({ message: 'Veri kaydedilemedi.', error });
   }
 });
-
-// Public klasörünü sunucuya bağla
-app.use(express.static('public'));
 
 // Sunucuyu başlat
 const PORT = 3000;
