d860670ecd9bcb946e088a62d8b7f548ffaa2676
YagTur123 <yagiz.efe.turan111@gmail.com>
2024-12-03 23:55:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Perfume = require('./models/perfume');
+const Perfume = require('./perfume');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-// MongoDB URI
 const mongoURI = 'mongodb+srv://yagiz:<db_yagiz123>@cluster0.vrkqh.mongodb.net/?retryWrites=true&w=majority';
 
-// MongoDB'ye bağlan
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB bağlantısı başarılı!'))
   .catch(err => console.error('MongoDB bağlantı hatası:', err));
 
-app.use(express.json());
-
-// Form verilerini kaydetme endpoint'i
-app.post('/api/add-perfume', async (req, res) => {
+app.get('/api/perfumes', async (req, res) => {
   try {
-    const perfume = new Perfume(req.body);
-    await perfume.save();
-    res.status(200).send({ message: 'Veri başarıyla kaydedildi!' });
-  } catch (error) {
-    res.status(500).send({ message: 'Veri kaydedilemedi.', error });
+    const perfumes = await Perfume.find();
+    res.json(perfumes);
+  } catch (err) {
+    res.status(500).send('Veriler alınırken hata oluştu.');
   }
 });
 
-// Sunucuyu başlat
+app.post('/api/add-perfume', async (req, res) => {
+  try {
+    const { perfumeName, essencePercent, ethanolPercent, waterPercent, lockCode } = req.body;
+
+    const newPerfume = new Perfume({ perfumeName, essencePercent, ethanolPercent, waterPercent, lockCode });
+    await newPerfume.save();
+
+    res.status(200).send('Parfüm verisi başarıyla eklendi!');
+  } catch (err) {
+    res.status(500).send('Veri eklenirken hata oluştu.');
+  }
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor.`));
